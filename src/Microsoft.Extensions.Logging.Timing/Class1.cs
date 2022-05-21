8eeb27ee87ddd4a8d164e9c59fbf214ba1412b22
Daniel Destouche <daniel.destouche@gmail.com>
2022-05-21 13:42:07 -0400

    Initial scaffolding

---
+++ src/Microsoft.Extensions.Logging.Timing/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Microsoft.Extensions.Logging.Timing;
+public class Class1
+{
+
+}
---
+++ test/Microsoft.Extensions.Logging.Timing.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Microsoft.Extensions.Logging.Timing.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
